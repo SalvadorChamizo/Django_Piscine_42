6debaaecfce28a6108f18a2cfb8afcc8d9beaef8
SalvadorChamizo <salvadorchamizo@gmail.com>
2026-03-16 23:25:30 +0100

    feat: Finish ex01

--- 0.1-Starting/ex01/numbers.py
+++ 0.1-Starting/ex01/numbers.py
@@ -4,9 +4,9 @@ def print_numbers(numbers):
 
 
 def open_file():
-    file = open("numbers.py", "r")
-    data = file.read()
-    number_list = [int(i) for i in data.split()]
+    with open("numbers.txt", "r") as file:
+        data = file.read()
+    number_list = [int(i) for i in data.split(',')]
     print_numbers(number_list)
 
 
