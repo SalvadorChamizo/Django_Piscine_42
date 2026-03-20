94e87323428d8813610eeb2cd16b510827f5a0d9
SalvadorChamizo <salvadorchamizo@gmail.com>
2026-03-20 02:34:15 +0100

    feat: Finished ex05 working as subject expect it

---
+++ 0.2-Oob/ex05/elem.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+
+class Text(str):
+    """
+    A Text class to represent a text you could use with your HTML elements.
+
+    Because directly using str class was too mainstream.
+    """
+
+    def __str__(self):
+        """
+        Do you really need a comment to understand this method?..
+        return super().__str__().replace('\n', '\n<br />\n')
+        """
+
+        s = super().__str__()
+        s = s.replace('&', '&amp;')
+        s = s.replace('<', '&lt;')
+        s = s.replace('>', '&gt;')
+        s = s.replace('"', '&quot;')
+        s = s.replace('\n', '\n<br />\n')
+        return s
+
+
+class Elem:
+    """
+    Elem will permit us to represent our HTML elements.
+    """
+    class ValidationError(Exception):
+        pass
+
+    def __init__(self, tag='div', attr={}, content=None, tag_type='double'):
+        """
+        __init__() method.
+
+        Obviously.
+        """
+        self.tag = tag
+        self.attr = attr
+        self.tag_type = tag_type
+
+        if content is None:
+            self.content = []
+        elif isinstance(content, list):
+            if not self.check_type(content):
+                raise Elem.ValidationError
+            self.content = [elem for elem in content if elem != Text('')]
+        elif self.check_type(content):
+            if content == Text(''):
+                self.content = []
+            else:
+                self.content = [content]
+        else:
+            raise Elem.ValidationError
+
+    def __str__(self):
+        """
+        The __str__() method will permit us to make a plain HTML representation
+        of our elements.
+        Make sure it renders everything (tag, attributes, embedded
+        elements...).
+        """
+        attr = self.__make_attr()
+
+        if self.tag_type == 'double':
+            content = self.__make_content()
+            result = f"<{self.tag}{attr}>{content}</{self.tag}>"
+        elif self.tag_type == 'simple':
+            result = f"<{self.tag}{attr} />"
+        return result
+
+    def __make_attr(self):
+        """
+        Here is a function to render our elements attributes.
+        """
+        result = ''
+        for pair in sorted(self.attr.items()):
+            result += ' ' + str(pair[0]) + '="' + str(pair[1]) + '"'
+        return result
+
+    def __make_content(self):
+        """
+        Here is a method to render the content, including embedded elements.
+        """
+
+        if len(self.content) == 0:
+            return ''
+
+        result = '\n'
+
+        for elem in self.content:
+            lines = str(elem).split('\n')
+            for line in lines:
+                result += '  ' + line + '\n'
+        return result
+
+    def add_content(self, content):
+        if not Elem.check_type(content):
+            raise Elem.ValidationError
+        if type(content) == list:
+            self.content += [elem for elem in content if elem != Text('')]
+        elif content != Text(''):
+            self.content.append(content)
+
+    @staticmethod
+    def check_type(content):
+        """
+        Is this object a HTML-compatible Text instance or a Elem, or even a
+        list of both?
+        """
+        return (isinstance(content, Elem) or type(content) == Text or
+                (type(content) == list and all([type(elem) == Text or
+                                                isinstance(elem, Elem)
+                                                for elem in content])))
+
+
+if __name__ == '__main__':
+    from elements import Html, Head, Body, Title, H1, Img
+    from elem import Text
+
+    page = Html([
+        Head(
+            Title(Text('"Hello ground!"'))
+        ),
+        Body([
+            H1(Text('"Oh no, not again!"')),
+            Img(attr={'src': 'http://i.imgur.com/pfp3T.jpg'})
+        ])
+    ])
+
+    print(page)
